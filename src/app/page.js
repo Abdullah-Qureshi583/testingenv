ae3de2cab8bb23cc2370e4d84637ac3cfac58e25
Abdullah-Qureshi583 <mabdullahqureshi583@gmail.com>
2024-11-21 22:32:13 +0500

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const a = process.env.MYVAR
+console.log("the name is:",a) // Output: myvalue
+const Home = () => {
+  return (
+    <div>
+      Home page
+    </div>
+  )
+}
+
+export default Home
