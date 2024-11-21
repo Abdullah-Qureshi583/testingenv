6b7ae107e04260bd64f52d52355a363cfef18bee
Abdullah-Qureshi583 <mabdullahqureshi583@gmail.com>
2024-11-21 22:51:08 +0500

    noo com

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+"use client"
 import React from 'react'
-const a = process.env.MYVAR
+const a = process.env.NEXT_PUBLIC_MYVAR
 console.log("the name is:",a) // Output: myvalue
 const Home = () => {
   return (
     <div>
-      Home page there is the lRGE content {a}
+      Home page there is the lRGE content 
     </div>
   )
 }
